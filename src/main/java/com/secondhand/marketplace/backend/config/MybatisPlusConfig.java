ba78d916139f3cb8e5e282ad15aa2a0df6d2f58a
lind <2022150181@email.szu.edu.cn>
2026-04-08 10:42:51 +0800

    code to lynn

---
+++ src/main/java/com/secondhand/marketplace/backend/config/MybatisPlusConfig.java
@@ -0,0 +1,11 @@
+package com.secondhand.marketplace.backend.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.secondhand.marketplace.backend.modules")
+public class MybatisPlusConfig {
+}
---
+++ src/test/java/com/secondhand/marketplace/backend/SecondhandMarketplaceBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.secondhand.marketplace.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketplaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
